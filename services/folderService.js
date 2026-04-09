35433006c94ea258a6393038e053cbcf8c5c2d3f
Emrah Isovic <emrahisovic20@gmail.com>
2026-04-09 19:40:53 +0200

    feat: added folder queries

---
+++ services/folderService.js
@@ -0,0 +1,89 @@
+import { prisma } from '../lib/prisma.js';
+
+const isFolderNameTaken = async ({ name, parentId, userId }) => {
+  if (parentId !== null && !parentId) throw new Error('parentId is required.');
+  if (!name) throw new Error('Name is required.');
+  if (!userId) throw new Error('UserId is required.');
+
+  const folder = await prisma.folder.findFirst({
+    where: {
+      name,
+      parentId: parentId || null,
+      userId,
+    },
+  });
+
+  return !!folder;
+};
+
+const getFolderByIdAndUserId = async ({ userId, id }) => {
+  if (!userId) throw new Error('UserId is required.');
+  if (id === undefined) throw new Error('folderId is required.');
+
+  if (id === null) {
+    return {
+      id: null,
+      name: null,
+      parentId: null,
+    };
+  }
+
+  const folder = await prisma.folder.findFirst({
+    where: {
+      userId,
+      id,
+    },
+  });
+
+  return folder;
+};
+
+const getFolderById = async ({ id }) => {
+  if (id === undefined) throw new Error('FolderId is required.');
+
+  const folder = await prisma.folder.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  return folder;
+};
+
+const getAllNestedFoldersByParentIdAndUserId = async ({ parentId, userId }) => {
+  if (parentId !== null && !parentId) throw new Error('parentId is required.');
+  if (!userId) throw new Error('UserId is required.');
+
+  const folders = await prisma.folder.findMany({
+    where: {
+      parentId,
+      userId,
+    },
+  });
+
+  return folders;
+};
+
+const addNewFolder = async ({ name, parentId, userId }) => {
+  if (parentId !== null && !parentId) throw new Error('parentId is required.');
+  if (!name) throw new Error('Name is required.');
+  if (!userId) throw new Error('UserId is required.');
+
+  const user = await prisma.folder.create({
+    data: {
+      name,
+      parentId,
+      userId,
+    },
+  });
+
+  return user;
+};
+
+export {
+  isFolderNameTaken,
+  addNewFolder,
+  getAllNestedFoldersByParentIdAndUserId,
+  getFolderByIdAndUserId,
+  getFolderById,
+};
